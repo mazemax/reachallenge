e6978dae73cf68a30d217e75d75ec371a39d7921
M.Saad Siddiqui <to.msaads@gmail.com>
2018-11-19 14:17:10 +0800

    Ensured that PLACE is the first command.

--- README.md
+++ README.md
@@ -19,13 +19,12 @@ Create an application that can read in commands of the following (textual) form:
 
 - PLACE will put the toy robot on the table in position X,Y and facing NORTH, SOUTH, EAST or WEST.
 - The origin (0,0) can be considered to be the SOUTH WEST most corner.
-- The first valid command to the robot is a PLACE command, after that, any
-  sequence of commands may be issued, in any order, including another PLACE command. The application should discard all commands in the sequence until a valid PLACE command has been executed.
+- The first valid command to the robot is a PLACE command, after that, any sequence of commands may be issued, in any order, including another PLACE command. The application should discard all commands in the sequence until a valid PLACE command has been executed.
 - MOVE will move the toy robot one unit forward in the direction it is currently facing.
 - LEFT and RIGHT will rotate the robot 90 degrees in the specified direction without changing the position of the robot.
 - REPORT will announce the X,Y and F of the robot. This can be in any form, but standard output is sufficient.
 
-- A robot that is not on the table can choose the ignore the MOVE, LEFT, RIGHT and REPORT commands.
+- A robot that is not on the table can choose to ignore the MOVE, LEFT, RIGHT and REPORT commands.
 - Input can be from a file, or from standard input, as the developer chooses.
 - Provide test data to exercise the application.
 - The application must be a command line application.
@@ -33,8 +32,7 @@ Create an application that can read in commands of the following (textual) form:
 Constraints
 -----------
 
-- The toy robot must not fall off the table during movement. This also
-  includes the initial placement of the toy robot.
+- The toy robot must not fall off the table during movement. This also includes the initial placement of the toy robot.
 - Any move that would cause the robot to fall must be ignored.
 
 Example Input and Output
--- src/index.js
+++ src/index.js
@@ -2,11 +2,32 @@ import Vorpal from "vorpal";
 import chalk from "chalk";
 
 const vorpal = new Vorpal();
+vorpal.localStorage('toyRobot' + new Date());
+
+const checkForLastCommand = (args) => {
+    // Checking last command
+    let commands = vorpal.localStorage.getItem('commands');
+    if (commands !== null) {
+        return true;
+    } else {
+        process.stdout.write(chalk.red("Error: The first valid command to the robot is a PLACE command! \n"));
+        return false;
+    }
+};
 
 vorpal.command("PLACE <X,Y,F>")
   .description("PLACE will put the toy robot on the table in position X,Y and facing NORTH, SOUTH, EAST or WEST.")
-  .action((args, callback) => {
+    .action((args, callback) => {
       try {
+          // Storing the PLACE command
+          let commands = vorpal.localStorage.getItem('commands');
+          if (commands !== null) {
+              Object.assign(JSON.parse(commands), { command: "PLACE" + args["X,Y,F"] });
+              vorpal.localStorage.setItem('commands', JSON.stringify(commands));
+          } else {
+              vorpal.localStorage.setItem('commands', JSON.stringify({ command: "PLACE" + args["X,Y,F"] }));
+          }
+
           let parts = args["X,Y,F"].split(",");
           if (parts.length !== 3) {
               process.stdout.write(chalk.red("Error: Require parameter in X,Y,F format!\n"));
@@ -22,12 +43,18 @@ vorpal.command("PLACE <X,Y,F>")
           process.stdout.write(chalk.red("Error: " + e.message + "\n"));
       }
       callback();
-  });
+    });
 
 vorpal.command("MOVE")
     .description("MOVE will move the toy robot one unit forward in the direction it is currently facing.")
+    .validate(checkForLastCommand)
     .action((args, callback) => {
         try {
+            // Storing the MOVE command
+            let commands = JSON.parse(vorpal.localStorage.getItem('commands'));
+            Object.assign(commands, { command: "MOVE" });
+            vorpal.localStorage.setItem('commands', JSON.stringify(commands));
+
             process.stdout.write(chalk.green("Moving 1 unit forward. \n"));
         } catch (e) {
             process.stdout.write(chalk.red("Error: " + e.message + "\n"));
@@ -37,8 +64,14 @@ vorpal.command("MOVE")
 
 vorpal.command("LEFT")
     .description("LEFT will rotate the robot 90 degrees in the specified direction without changing the position of the robot.")
+    .validate(checkForLastCommand)
     .action((args, callback) => {
         try {
+            // Storing the LEFT command
+            let commands = JSON.parse(vorpal.localStorage.getItem('commands'));
+            Object.assign(commands, { command: "LEFT" });
+            vorpal.localStorage.setItem('commands', JSON.stringify(commands));
+
             process.stdout.write(chalk.green("Turning left. \n"));
         } catch (e) {
             process.stdout.write(chalk.red("Error: " + e.message + "\n"));
@@ -48,8 +81,14 @@ vorpal.command("LEFT")
 
 vorpal.command("RIGHT")
     .description("RIGHT will rotate the robot 90 degrees in the specified direction without changing the position of the robot.")
+    .validate(checkForLastCommand)
     .action((args, callback) => {
         try {
+            // Storing the RIGHT command
+            let commands = JSON.parse(vorpal.localStorage.getItem('commands'));
+            Object.assign(commands, { command: "RIGHT" });
+            vorpal.localStorage.setItem('commands', JSON.stringify(commands));
+
             process.stdout.write(chalk.green("Turning Right. \n"));
         } catch (e) {
             process.stdout.write(chalk.red("Error: " + e.message + "\n"));
@@ -59,8 +98,14 @@ vorpal.command("RIGHT")
 
 vorpal.command("REPORT")
     .description("REPORT will announce the X,Y and F of the robot.")
+    .validate(checkForLastCommand)
     .action((args, callback) => {
         try {
+            // Storing the REPORT command
+            let commands = JSON.parse(vorpal.localStorage.getItem('commands'));
+            Object.assign(commands, { command: "REPORT" });
+            vorpal.localStorage.setItem('commands', JSON.stringify(commands));
+
             process.stdout.write(chalk.green("Announcing X,Y and F. \n"));
         } catch (e) {
             process.stdout.write(chalk.red("Error: " + e.message + "\n"));
